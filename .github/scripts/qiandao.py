e3ed6ef2e1b337a3aebd8e046b762719b4978ab3
He Rongyun <124922326+5tsv@users.noreply.github.com>
2025-04-01 06:56:50 +0800

    Update qiandao.py

--- .github/scripts/qiandao.py
+++ .github/scripts/qiandao.py
@@ -47,6 +47,7 @@ class bbsClient:
 
 	def login(self):
 		"""Login with username and password"""
+		self.invite()
 		loginhash, formhash = self.login_form_hash()
 		login_url = f'https://{self.hostname}/member.php?mod=logging&action=login&loginsubmit=yes&loginhash={loginhash}&inajax=1'
 		headers = copy(self._common_headers)
@@ -92,14 +93,14 @@ class bbsClient:
 		checkinUrl=f'https://{self.hostname}/plugin.php?id=k_misign:sign&operation=qiandao&formhash={self.formhash}&inajax=1'
 		return self.session.get(checkinUrl).text
 	def invite(self):
-		requests.get(f'https://{self.hostname}/',params={'fromuser':self.username})
+		response=requests.get(f'https://{self.hostname}/',params={'fromuser':self.username})
+		logger.info(response.url)
 
 if __name__ == '__main__':
 	try:
 		url = 'https://'+os.environ.get('HOSTNAME')
 		logger.info(f'{url}')
 		client = bbsClient(urlparse(url).hostname, os.environ.get('USERNAME'), os.environ.get('PASSWORD'))
-		client.invite()
 		client.login()
 		result = client.checkin()
 		logger.info(result)
