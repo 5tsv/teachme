e781a1b56628b23221aa25d2246c459bc6726f15
He Rongyun <124922326+5tsv@users.noreply.github.com>
2025-04-01 07:08:28 +0800

    Update qiandao.py

--- .github/scripts/qiandao.py
+++ .github/scripts/qiandao.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
 		logger.info(f'{url}')
 		client = bbsClient(urlparse(url).hostname, os.environ.get('USERNAME'), os.environ.get('PASSWORD'))
 		result=client.invite()
-		logger.info(result.url)
+		logger.info(resultl)
 		client.login()
 		result = client.checkin()
 		logger.info(result)
